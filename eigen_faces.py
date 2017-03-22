b2ba8ae04ec70b6cb0f5bf7162cf11a7372a0ee3
Matt Brucker <mpbrucker@gmail.com>
2017-03-22 18:51:36 -0400

    Implemented testing of the eigenface method using test faces.

--- eigen_faces.py
+++ eigen_faces.py
@@ -9,15 +9,15 @@ import scipy.io as io
 import matplotlib.pyplot as plt
 
 
-def parse_faces(file_name='face_detect.mat'):
-    faces = io.loadmat(file_name)
-    imgs = faces['faces_train']
+def parse_faces(data='faces_train'):
+    faces = io.loadmat('face_detect.mat')
+    imgs = faces[data]
     all_train = np.reshape(imgs, (65536, imgs.shape[2]))
     return all_train
 
 
-def get_unique_labels(file_name='face_detect.mat'):
-    labels = get_labels(file_name)
+def get_unique_labels(data='names_train'):
+    labels = get_labels(data)
     unique_labels = []
     for label in labels:
         if label not in unique_labels:
@@ -25,15 +25,16 @@ def get_unique_labels(file_name='face_detect.mat'):
     return unique_labels
 
 
-def get_labels(file_name='face_detect.mat'):
-    data = io.loadmat(file_name)
-    all_labels = data['names_train']
-    label_list = [''] * 240
+def get_labels(data_type='names_train'):
+    data = io.loadmat('face_detect.mat')
+    all_labels = data[data_type]
+    label_list = [''] * len(all_labels[0])
     for label in all_labels:
         for idx, c in enumerate(label):
             label_list[idx] += c
     label_list = [label.rstrip("\x00") for label in label_list]
     return label_list
+    # print(classes)
 
 
 def get_face_classes(components):
@@ -46,9 +47,6 @@ def get_face_classes(components):
             classes[all_labels[idx]] = face
         else:
             classes[all_labels[idx]] += face
-    print(classes['Adam Selker'])
-    print(get_mean_face(components[:, :6]))
-    # print(classes)
     return classes
 
 
@@ -56,8 +54,8 @@ def get_closest_face(subspace_face, base_faces):
     classes = get_face_classes(base_faces)
     dists = {key: 0 for key in classes.keys()}
     for key in classes.keys():
-        dists[key] = get_face_distance(subspace_face, classes[key])
-    print(dists)
+        dist = get_face_distance(subspace_face, classes[key][:, None])
+        dists[key] = dist
     return min(dists, key=dists.get)
 
 
@@ -68,12 +66,15 @@ def get_mean_face(face_set):
     return np.array(np.divide(np.sum(face_set, axis=1), face_set.shape[1]))[:, None]  # Reshapes into 2D array
 
 
-def get_standardized_faces(all_faces):
+def get_standardized_faces(all_faces, provided_mean=None):
     """
     Returns the matrix of faces centered around the mean
     """
-    mean_face = get_mean_face(all_faces)  # Gets the mean face
-    imgs_stand = faces-mean_face  # Turns mean_face from a 1D array to a 2D array
+    if provided_mean is not None:
+        mean_face = provided_mean
+    else:
+        mean_face = get_mean_face(all_faces)  # Gets the mean face
+    imgs_stand = all_faces-mean_face  # Turns mean_face from a 1D array to a 2D array
     return imgs_stand
 
 
@@ -109,19 +110,29 @@ def get_face_space_distance(face, eigen_faces):
 
 
 if __name__ == '__main__':
-    get_labels()
     faces = parse_faces()
     eigen_faces = find_eigen_faces(faces, 200)
     mean_face = get_mean_face(faces)
     consts = get_face_projections(get_standardized_faces(faces), eigen_faces)
 
-    print(get_closest_face(get_face_projections(faces[:, 156], eigen_faces), consts))
+    test_faces_easy = parse_faces('faces_test_hard')
+    test_consts = get_face_projections(get_standardized_faces(test_faces_easy, mean_face), eigen_faces)
+    test_labels = get_labels(data_type='names_test_hard')
 
-    face_reconstruct = np.dot(eigen_faces, consts[:,1])[:,None] + mean_face
+    correct = 0
+
+    for idx, face in enumerate(test_consts.T):
+        guess = get_closest_face(face[:, None], consts)
+        if test_labels[idx] == guess:
+            correct += 1
+        print(idx)
+    correct /= test_faces_easy.shape[1]
+    print("Correct: ", correct)
+    face_reconstruct = np.dot(eigen_faces, test_consts[:,1])[:,None] + mean_face
     # test_face = get_face_projections(get_standardized_faces(faces)[:,1], eigen_faces)
     # test_result = get_closest_face(test_face, consts)
     # print(test_result)
 
-    face = np.reshape(face_reconstruct, (256, 256))
+    face = np.reshape(test_faces_easy[:,39], (256, 256))
     plt.imshow(face, cmap="gray")
     plt.show()
